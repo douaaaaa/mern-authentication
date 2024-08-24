507001ddac9ea1326d7d129bbd176ad36628a37c
Douâa Bouabdallah <bouabdallehdouaa@gmail.com>
2024-08-24 21:10:50 +0100

    Login UI

--- frontend/src/Pages/SignUpPage.jsx
+++ frontend/src/Pages/SignUpPage.jsx
@@ -47,16 +47,16 @@ function SignUpPage() {
             value={Password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          <motion.button
+            className=" mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600  text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.98 }}
+            type="submit"
+          >
+            Sign Up
+          </motion.button>
         </form>
         <PasswordMeter password={Password} />
-        <motion.button
-          className=" mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600  text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
-          whileHover={{ scale: 1.02 }}
-          whileTap={{ scale: 0.98 }}
-          type="submit"
-        >
-          Sign Up
-        </motion.button>
       </div>
       <div className=" px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
         <p className=" text-sm text-gray-400">
